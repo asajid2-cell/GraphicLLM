dcf71431a0c378a5766d40370e0853af5b9a7ca1
ahmed-sajid <ahmedsajid2k3@outlook.com>
2026-05-11 17:21:35 -0600

    Move render graph validation clears into pass

--- CortexEngine/src/Graphics/Passes/RenderGraphValidationPass.h
+++ CortexEngine/src/Graphics/Passes/RenderGraphValidationPass.h
@@ -1,20 +1,38 @@
 #pragma once
 
 #include "Graphics/RenderGraph.h"
+#include "Graphics/RHI/DescriptorHeap.h"
+#include "Utils/Result.h"
 
+#include <array>
 #include <functional>
 
 namespace Cortex::Graphics::RenderGraphValidationPass {
 
-using StageCallback = std::function<void(ID3D12GraphicsCommandList*, const RenderGraph&, RGResourceHandle)>;
+struct TransientValidationViews {
+    DescriptorHandle rtvA;
+    DescriptorHandle srvA;
+    DescriptorHandle rtvB;
+    DescriptorHandle srvB;
+};
+
+struct TransientValidationTarget {
+    ID3D12Device* device = nullptr;
+    DescriptorHeapManager* descriptorManager = nullptr;
+    DescriptorHandle rtv;
+    DescriptorHandle srv;
+    std::array<float, 4> clearColor = {};
+    const char* descriptorFailureReason = nullptr;
+};
 
 struct TransientValidationContext {
     RGResourceDesc transientDesc;
-    StageCallback passA;
-    StageCallback passB;
+    TransientValidationTarget passA;
+    TransientValidationTarget passB;
     std::function<void(const char*)> failStage;
 };
 
+[[nodiscard]] Result<TransientValidationViews> CreateTransientValidationViews(DescriptorHeapManager* descriptorManager);
 [[nodiscard]] bool AddTransientValidation(RenderGraph& graph, const TransientValidationContext& context);
 
 } // namespace Cortex::Graphics::RenderGraphValidationPass

7763434f3b6b5b5e020b611848e6ae8e02f3b941
ahmed-sajid <ahmedsajid2k3@outlook.com>
2026-05-11 12:12:48 -0600

    Move HZB build dispatch into pass

--- CortexEngine/src/Graphics/Passes/HZBPass.h
+++ CortexEngine/src/Graphics/Passes/HZBPass.h
@@ -22,6 +22,34 @@ struct GraphContext {
     uint32_t mipCount = 0;
 };
 
+struct ResourceStateRef {
+    ID3D12Resource* resource = nullptr;
+    D3D12_RESOURCE_STATES* state = nullptr;
+};
+
+struct BuildContext {
+    ID3D12GraphicsCommandList* commandList = nullptr;
+    DescriptorHeapManager* descriptorManager = nullptr;
+    ResourceStateRef depth{};
+    ResourceStateRef hzb{};
+    ID3D12RootSignature* compactRootSignature = nullptr;
+    DX12ComputeRootSignature* fallbackRootSignature = nullptr;
+    DX12ComputePipeline* initPipeline = nullptr;
+    DX12ComputePipeline* downsamplePipeline = nullptr;
+    D3D12_GPU_VIRTUAL_ADDRESS frameConstants = 0;
+    DescriptorHandle depthSrv{};
+    std::span<const DescriptorHandle> mipSrvStaging;
+    std::span<const DescriptorHandle> mipUavStaging;
+    std::span<const DescriptorHandle> dispatchSrvTable;
+    std::span<const DescriptorHandle> dispatchUavTable;
+    bool dispatchTablesValid = false;
+    uint32_t width = 0;
+    uint32_t height = 0;
+    uint32_t mipCount = 0;
+};
+
+[[nodiscard]] bool BuildFromDepth(const BuildContext& context);
+
 void AddFromDepth(RenderGraph& graph,
                   RGResourceHandle depthHandle,
                   RGResourceHandle hzbHandle,
